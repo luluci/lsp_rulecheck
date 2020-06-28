e7695a49e1703bf60a65efdb78754d4d714461d9
luluci <lunacied@gmail.com>
2020-06-28 17:41:55 +0900

    serverのコンソールログ出るようになったよ

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -2,48 +2,91 @@
 
 import * as path from "path";
 import { ExtensionContext, window as Window } from "vscode";
-//import { workspace, commands, OutputChannel } from 'vscode';
+import { workspace, commands, OutputChannel } from 'vscode';
 import { LanguageClient, LanguageClientOptions, RevealOutputChannelOn, ServerOptions, TransportKind } from "vscode-languageclient";
+import * as WebSocket from 'ws';
 
 let client: LanguageClient;
 
 export function activate(context: ExtensionContext): void {
-    const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
-    const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"], cwd: process.cwd() };
-    const serverOptions: ServerOptions = {
-        run: { module: serverModule, transport: TransportKind.ipc, options: { cwd: process.cwd() } },
-        debug: {
-            module: serverModule,
-            transport: TransportKind.ipc,
-            options: debugOptions,
-        },
-    };
-    const clientOptions: LanguageClientOptions = {
-        documentSelector: [
-            {
-                scheme: "file",
-                language: "plaintext",
-            },
-            {
-                scheme: "file",
-                language: "markdown",
-            }],
-        diagnosticCollectionName: "sample",
+
+	const serverModule = context.asAbsolutePath(path.join("server", "out", "server.js"));
+	const debugOptions = { execArgv: ["--nolazy", "--inspect=6009"], cwd: process.cwd() };
+	const serverOptions: ServerOptions = {
+		run: { module: serverModule, transport: TransportKind.ipc, options: { cwd: process.cwd() } },
+		debug: {
+			module: serverModule,
+			transport: TransportKind.ipc,
+			options: debugOptions,
+		},
+	};
+
+	// デバッグ用
+	const socketPort = workspace.getConfiguration('languageServerExample').get('port', 7000);
+	let socket: WebSocket | null = null;
+	
+	commands.registerCommand('languageServerExample.startStreaming', () => {
+		// Establish websocket connection
+		socket = new WebSocket(`ws://localhost:${socketPort}`);
+	});
+	// The log to send
+	let log = '';
+	const websocketOutputChannel: OutputChannel = {
+		name: 'websocket',
+		// Only append the logs but send them later
+		append(value: string) {
+			log += value;
+			console.log(value);
+		},
+		appendLine(value: string) {
+			log += value;
+			// Don't send logs until WebSocket initialization
+			if (socket && socket.readyState === WebSocket.OPEN) {
+				socket.send(log);
+			}
+			log = '';
+		},
+		clear() {},
+		show() {},
+		hide() {},
+		dispose() {}
+	};
+	// デバッグ用
+
+	const clientOptions: LanguageClientOptions = {
+		documentSelector: [
+			{
+				scheme: "file",
+				language: "plaintext",
+			},
+			{
+				scheme: "file",
+				language: "markdown",
+			}],
+		diagnosticCollectionName: "sample",
 		revealOutputChannelOn: RevealOutputChannelOn.Never,
-		outputChannelName: 'sample_output_channel',
-    };
 
-    try {
-        client = new LanguageClient("Sample LSP Server", serverOptions, clientOptions);
-    } catch (err) {
-        Window.showErrorMessage("The extension couldn't be started. See the output channel for details.");
+		// Hijacks all LSP logs and redirect them to a specific port through WebSocket connection
+		outputChannel: websocketOutputChannel
+	};
 
-        return;
-    }
-    client.registerProposedFeatures();
 
-    context.subscriptions.push(
-        client.start(),
+	try {
+		client = new LanguageClient(
+			'languageServerExample',
+			"Sample LSP Server",
+			serverOptions,
+			clientOptions
+		);
+	} catch (err) {
+		Window.showErrorMessage("The extension couldn't be started. See the output channel for details.");
+
+		return;
+	}
+	client.registerProposedFeatures();
+
+	context.subscriptions.push(
+		client.start(),
 	);
 
 	console.log("client: start!!");
--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,7 @@ import {
 	createConnection,
 	Diagnostic,
 	DiagnosticSeverity,
+	ProposedFeatures,
 	Range,
 	TextDocuments,
 	TextDocumentEdit,
@@ -19,8 +20,12 @@ namespace CommandIDs {
 }
 // Create a connection for the server. The connection uses Node's IPC as a transport.
 // Also include all preview / proposed LSP features.
-const connection = createConnection();
+//const connection = createConnection();
+const connection = createConnection(ProposedFeatures.all);
 connection.console.info(`Sample server running in node ${process.version}`);
+
+// Create a simple text document manager. The text document manager
+// supports full document sync only
 let documents!: TextDocuments<TextDocument>;
 
 connection.onInitialize(() => {
@@ -60,87 +65,104 @@ function validate(doc: TextDocument) {
 	connection.sendDiagnostics({ uri: doc.uri, diagnostics });
 	*/
 
-    // ２つ以上並んでいるアルファベット大文字を検出
-    const text = doc.getText();
-    // 検出するための正規表現 (正規表現テスト: https://regex101.com/r/wXZbr9/1)
-    const pattern = /\b[A-Z]{2,}\b/g;
-    let m: RegExpExecArray | null;
+	// ２つ以上並んでいるアルファベット大文字を検出
+	const text = doc.getText();
+	// 検出するための正規表現 (正規表現テスト: https://regex101.com/r/wXZbr9/1)
+	const pattern = /\b[A-Z]{2,}\b/g;
+	let m: RegExpExecArray | null;
 
-    // 警告などの状態を管理するリスト
-    const diagnostics: Diagnostic[] = [];
-    // 正規表現に引っかかった文字列すべてを対象にする
-    while ((m = pattern.exec(text)) !== null) {
-        // 対象の位置から正規表現に引っかかった文字列までを対象にする
-        const range: Range = {start: doc.positionAt(m.index),
-                              end: doc.positionAt(m.index + m[0].length),
-        };
-        // 警告内容を作成，上から範囲，メッセージ，重要度，ID，警告原因
-        const diagnostic: Diagnostic = Diagnostic.create(
-            range,
-            `${m[0]} is all uppercase.`,
-            DiagnosticSeverity.Warning,
-            "",
-            "sample",
-        );
-        // 警告リストに警告内容を追加
-        diagnostics.push(diagnostic);
-    }
+	// 警告などの状態を管理するリスト
+	const diagnostics: Diagnostic[] = [];
+	// 正規表現に引っかかった文字列すべてを対象にする
+	while ((m = pattern.exec(text)) !== null) {
+		// 対象の位置から正規表現に引っかかった文字列までを対象にする
+		const range: Range = {start: doc.positionAt(m.index),
+							  end: doc.positionAt(m.index + m[0].length),
+		};
+		// 警告内容を作成，上から範囲，メッセージ，重要度，ID，警告原因
+		const diagnostic: Diagnostic = Diagnostic.create(
+			range,
+			`${m[0]} is all uppercase.`,
+			DiagnosticSeverity.Warning,
+			"",
+			"sample",
+		);
+		// 警告リストに警告内容を追加
+		diagnostics.push(diagnostic);
+	}
 
-    // Send the computed diagnostics to VSCode.
-    connection.sendDiagnostics({ uri: doc.uri, diagnostics });
+	// Send the computed diagnostics to VSCode.
+	connection.sendDiagnostics({ uri: doc.uri, diagnostics });
 }
 
 function setupDocumentsListeners() {
 	documents.listen(connection);
 
+	let debug_onDonDidOpen = 0;
 	// ドキュメントを開いたとき？
 	documents.onDidOpen((event) => {
 		validate(event.document);
+
+		//connection.console.log('onDidOpen');
+		console.log(`server: onDidOpen(${debug_onDonDidOpen})`);
+		debug_onDonDidOpen++;
 	});
 
+	let debug_onDidChangeContent = 0;
 	// ドキュメントが変更されたとき
 	documents.onDidChangeContent((change) => {
 		validate(change.document);
+
+		//connection.console.log('onDidChangeContent');
+		console.log(`server: onDidChangeContent(${debug_onDidChangeContent})`);
+		debug_onDidChangeContent++;
 	});
 
 	documents.onDidClose((close) => {
 		connection.sendDiagnostics({ uri: close.document.uri, diagnostics: []});
 	});
 
-    // Code Actionを追加する
-    connection.onCodeAction((params) => {
-        // sampleから生成した警告のみを対象とする
-        const diagnostics = params.context.diagnostics.filter((diag) => diag.source === "sample");
-        // 対象ファイルを取得する
-        const textDocument = documents.get(params.textDocument.uri);
-        if (textDocument === undefined || diagnostics.length === 0) {
-            return [];
-        }
-        const codeActions: CodeAction[] = [];
-        // 各警告に対してアクションを生成する
-        diagnostics.forEach((diag) => {
-            // アクションの目的
-            const title = "Fix to lower case";
-            // 警告範囲の文字列取得
-            const originalText = textDocument.getText(diag.range);
-            // 該当箇所を小文字に変更
-            const edits = [TextEdit.replace(diag.range, originalText.toLowerCase())];
-            const editPattern = { documentChanges: [
-                TextDocumentEdit.create({uri: textDocument.uri,
-                                         version: textDocument.version},
-                                        edits)] };
-            // コードアクションを生成
-            const fixAction = CodeAction.create(title,
-                                                editPattern,
-                                                CodeActionKind.QuickFix);
-            // コードアクションと警告を関連付ける
-            fixAction.diagnostics = [diag];
-            codeActions.push(fixAction);
-        });
+	// Code Actionを追加する
+	connection.onCodeAction((params) => {
+		// sampleから生成した警告のみを対象とする
+		const diagnostics = params.context.diagnostics.filter((diag) => diag.source === "sample");
+		// 対象ファイルを取得する
+		const textDocument = documents.get(params.textDocument.uri);
+		if (textDocument === undefined || diagnostics.length === 0) {
+			return [];
+		}
+		const codeActions: CodeAction[] = [];
+		// 各警告に対してアクションを生成する
+		diagnostics.forEach((diag) => {
+			// アクションの目的
+			const title = "Fix to lower case";
+			// 警告範囲の文字列取得
+			const originalText = textDocument.getText(diag.range);
+			// 該当箇所を小文字に変更
+			const edits = [TextEdit.replace(diag.range, originalText.toLowerCase())];
+			const editPattern = { documentChanges: [
+				TextDocumentEdit.create({uri: textDocument.uri,
+										 version: textDocument.version},
+										edits)] };
+			// コードアクションを生成
+			const fixAction = CodeAction.create(title,
+												editPattern,
+												CodeActionKind.QuickFix);
+			// コードアクションと警告を関連付ける
+			fixAction.diagnostics = [diag];
+			codeActions.push(fixAction);
+		});
 
-        return codeActions;
-    });
+		return codeActions;
+	});
 
+
+
+	connection.onDidChangeWatchedFiles(_change => {
+		// Monitored files have change in VSCode
+		connection.console.log('We received an file change event');
+	});
+	
 }
 
 console.log("server: start!!");
